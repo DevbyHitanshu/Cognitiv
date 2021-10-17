5507d2d57a735ac8010162cae4713ba36544919c
Hitanshu Kapoor <khitanshu4@gmail.com>
2021-10-17 18:20:27 +0530

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -1,4 +1,5 @@
 from os import name
+from re import L
 import mysql.connector
 import nltk
 from nltk.tokenize import word_tokenize
@@ -45,13 +46,46 @@ def my_function(var):
  
 ques=my_function(var1)
 print(ques)
+query1=speechtotext.speech()
 
-if ques==1:
+text = query1
+var2 = word_tokenize(text)
+print(var2)
+def my1_function(var):
+     J = 0 
+     L = 0 
+     for i in range (len(var)):
+         if ques==1 and var[i] == "tiger" :
+             J = 1
+         elif ques==1 :
+             J = 4
+         if ques==2 and var[i] == "A" and var[i+1] == "street" and var[i+2] == "delhi" and var[i+3] == "India" :
+             J = 2
+         elif ques==2:
+             J = 5
+         if ques==3 and var[i] == "782-717-6615" :
+             J = 3
+         elif ques==3:
+             J = 6
+
+            
+             
+     return J
+
+ans=my1_function(var2)
+print(ans)
+
+if ans==4:
     pyobj=pyttsx3.init()
-    pyobj.say("raj")  
+    pyobj.say("Tiger woods")  
     pyobj.runAndWait()
-if ques==2:
+if ans==5:
     pyobj=pyttsx3.init()
-    pyobj.say("10 shreshtha viha")  
+    pyobj.say("a Street Delhi India")  
     pyobj.runAndWait()
+if ans==6:
+    pyobj=pyttsx3.init()
+    pyobj.say("7827176615")
+    pyobj.runAndWait()
+
 
